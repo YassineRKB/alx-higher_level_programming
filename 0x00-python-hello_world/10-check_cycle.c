783d1ed60f6c759e3617617de0f40ce7a681f4be
Yassine Rakibi <yassine@arcraven.com>
2023-06-05 15:14:58 +0200

    10-check_cycle Home Sweet Home

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - function to check if linked list has loop
+ * @list: first node of linked list
+ * Return: 0 if there is no cycle, else 1
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	if(!list)
+		return (0);
+	
+	while (fast)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
