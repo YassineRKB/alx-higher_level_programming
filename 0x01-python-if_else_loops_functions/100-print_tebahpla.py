cb89780838933eb1f7318c14999f14742272de66
Yassine Rakibi <yassine@arcraven.com>
2023-06-07 02:44:11 +0200

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+flag = -1
+for i in range(122, 96, -1):
+    if flag > 0:
+        r = i - 32
+    else:
+        r = i
+    print("{}".format(chr(r)),end="")
+    flag = -1 * flag
