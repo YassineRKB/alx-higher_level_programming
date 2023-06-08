33044626f0af78bde9c982f4523a253101ba873e
Yassine Rakibi <yassine@arcraven.com>
2023-06-08 23:39:46 +0200

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 from sys import argv
 if __name__ == "__main__":
     nargs = len(argv)
-    print("{} {}".format(nargs - 1, "arguments"), end="")
+    print("{} {}".format(nargs - 1, "argument"), end="")
     if nargs > 1:
-        print(":")
+        print({True: ":", False: "s:"} [nargs == 2])
         for i in range(1, nargs):
             print("{}: {}".format(i, argv[i]))
     else:
-        print(".")
+        print("s.")
