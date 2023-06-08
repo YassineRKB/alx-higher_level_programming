ec2eadcd236faf4db3145dc2841d5182a846eb69
Yassine Rakibi <yassine@arcraven.com>
2023-06-09 00:40:37 +0200

    100-my_calculator.py: fixed typo

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
     nargs = len(argv)
     if nargs != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
