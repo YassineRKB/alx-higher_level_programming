b2bb072432ecd3f119457169d7da15644128abd4
Yassine Rakibi <yassine@arcraven.com>
2023-06-08 23:40:19 +0200

    2-args.py: fix white space

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     nargs = len(argv)
     print("{} {}".format(nargs - 1, "argument"), end="")
     if nargs > 1:
-        print({True: ":", False: "s:"} [nargs == 2])
+        print({True: ":", False: "s:"}[nargs == 2])
         for i in range(1, nargs):
             print("{}: {}".format(i, argv[i]))
     else:
