b06eceb67cda525ee7b5b5b0333ecbf881adc1e7
Yassine Rakibi <yassine@arcraven.com>
2023-06-08 22:55:25 +0200

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-r = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, r))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    r = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, r))
