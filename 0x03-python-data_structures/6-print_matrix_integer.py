22e00ab247685f8c83880d84f47e005821966c2b
Yassine Rakibi <yassine@arcraven.com>
2023-06-12 10:09:55 +0200

    6-print_matrix_integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for list in matrix:
+        listlen = len(list) - 1
+        nint = 0
+        for integ in list:
+            if nint != listlen:
+                print("{:d}".format(integ), end=" ")
+            else:
+                print("{:d}".format(integ))
+            nint += 1
