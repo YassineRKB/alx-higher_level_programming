d61f3f79d6d1fd57e212d32d1df68baca5d9cba5
Yassine Rakibi <yassine@arcraven.com>
2023-06-12 13:06:42 +0200

    8-multiple_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     slen = len(sentence)
-    fchar = sentence[0]
+    if slen == 0:
+        fchar = None
+    else:
+        fchar = sentence[0]
     res = (slen, fchar)
     return res
