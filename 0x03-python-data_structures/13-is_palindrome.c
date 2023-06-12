295fee4e0b0576ef4efa055b61c51677b535dcd0
Yassine Rakibi <yassine@arcraven.com>
2023-06-12 15:35:24 +0200

    13-is_palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,7 +7,7 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *lenlist = *head, *list = *head;
-	int *data, len = 0, i = 0;
+	int data[1024], len = 0, i = 0;
 
 	if (!head)
 		return (0);
@@ -16,20 +16,13 @@ int is_palindrome(listint_t **head)
 	len = calclen(lenlist);
 	if (i == 1)
 		return (1);
-	data = malloc(sizeof(int) * len);
-	if (!data)
-		return (0);
 	for (; list; i++, list = list->next)
 		data[i] = list->n;
 	for (i = 0; i <= (len / 2); i++)
 	{
 		if (data[i] != data[len - i - 1])
-		{
-			free(data);
 			return (0);
-		}
 	}
-	free(data);
 	return (1);
 }
 int calclen(listint_t *list)
