e99ed0c8d416b33d9bb1d384b31b0d411b55b504
Yassine Rakibi <yassine@arcraven.com>
2023-06-12 14:48:05 +0200

    13-is_palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * is_palindrome - func to check if list is palindrome
+ * @head: header of the linked list
+ * Return: 1 if palindrome or 0 otherwise
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *lenlist = *head, *list = *head;
+	int *data, len = 0, i = 0;
+
+	if (!head)
+		return (0);
+	if (!list)
+		return (1);
+	for (; lenlist; len++)
+		lenlist = lenlist->next;
+	data = malloc(sizeof(int) * len);
+	if (!data)
+		return (0);
+	for (; list; i++, list = list->next)
+		data[i] = list->n;
+	for (i = 0; i <= (len / 2); i++)
+	{
+		if (data[i] != data[len - i - 1])
+		{
+			free(data);
+			return (0);
+		}
+	}
+	free(data);
+	return (1);
+}
