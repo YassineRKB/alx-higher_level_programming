d00e863b6c88b6e783583ee48778a8771794c8d5
Yassine Rakibi <yassine@arcraven.com>
2023-06-14 20:38:04 +0200

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    ncset = set_1.union(set_2).difference(set_2 & set_1)
-    
+    ncset = set_1.union(set_2).difference(set_2 & set_1)    
     return ncset
