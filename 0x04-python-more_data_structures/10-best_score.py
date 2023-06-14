af11a9c1b9e8287e7f9d86696f5ae2951bba33b9
Yassine Rakibi <yassine@arcraven.com>
2023-06-14 23:29:57 +0200

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    bint = None
+    lastkey = None
+    for key in a_dictionary.keys():
+        if bint is None:
+            bint = a_dictionary[key]
+            lastkey = key
+        if bint < a_dictionary[key]:
+            bint = a_dictionary[key]
+            lastkey = key
+    return lastkey
