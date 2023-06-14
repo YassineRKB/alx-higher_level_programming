74d262ae45b15cca49f353fc39d2d55a8e77ba59
Yassine Rakibi <yassine@arcraven.com>
2023-06-14 19:21:23 +0200

    0-square_matrix_simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     matrixlen = len(matrix)
     neo = []
     for i in range(matrixlen):
-        sqrootmap = lambda x: x ** 2, matrix[i]
-        neo.append(list(map(sqrootmap)))
+        sqrootmap = lambda x: x ** 2
+        neo.append(list(map(sqrootmap, matrix[i])))
     return neo
