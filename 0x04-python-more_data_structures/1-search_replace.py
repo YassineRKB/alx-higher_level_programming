12eedcd7fc9b414b5784d0f6b8d67d5495f5f335
Yassine Rakibi <yassine@arcraven.com>
2023-06-14 19:37:59 +0200

    1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     newList = list()
-    for i in range(len(my_list)):
+    for i in my_list:
         if i != search:
             newList.append(i)
         else:
             newList.append(replace)
     return newList
-
