3ef6910dbabc173cbcc75552e9fd5c1af26fda7e
Yassine Rakibi <yassine@arcraven.com>
2023-06-27 01:17:16 +0200

    100-safe_print_integer_err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     verdict = True
     try:
         print("{:d}".format(value))
     except Exception as e:
         sys.stderr.write("Exception: " + str(e) + '\n')
+        sys.stderr.flush()
         verdict = False
     return verdict
