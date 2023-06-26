3a95c39a59223d09ca7b6282afd2481dbc5af770
Yassine Rakibi <yassine@arcraven.com>
2023-06-26 21:07:43 +0200

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            num += 1
+        except IndexError:
+            break
+    print()
+    return num
