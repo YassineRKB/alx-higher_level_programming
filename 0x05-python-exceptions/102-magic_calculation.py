2e652ec22bb915a60bd68bb15b9a5b0a4e839bc9
Yassine Rakibi <yassine@arcraven.com>
2023-06-27 01:32:07 +0200

    102-magic_calculations

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    r = 0
+    for i in range(1,3):
+        try:
+            if a > i:
+                raise Exception('Too far')
+            r = r + ((a ** b) / i)
+        except Exception:
+            r = a + b
+            break
+    return r
