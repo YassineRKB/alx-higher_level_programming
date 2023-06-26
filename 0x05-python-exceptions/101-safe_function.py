62053a02cfb84375686cde707c12226f17b4131f
Yassine Rakibi <yassine@arcraven.com>
2023-06-27 01:23:45 +0200

    101-safe_function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    res = 0
+    try:
+        res = fct(args)
+    except Exception as e:
+        sys.stderr.write("Exception: " + str(e) + '\n')
+        sys.stderr.flush()
+        res = None
+    return res
