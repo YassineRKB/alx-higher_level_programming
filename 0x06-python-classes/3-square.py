480908621f1778381b584af0901850d1cb70b607
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 01:37:49 +0200

    3-square - white space

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,7 +12,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self._Square__size = size
-    
+
     def area(self):
         sqreArea = self._Square__size ** 2
         return sqreArea
