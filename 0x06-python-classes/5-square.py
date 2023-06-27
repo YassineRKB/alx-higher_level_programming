f67e6daff2c17be9fe7f5d091acd1670b0134802
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 01:52:18 +0200

    5-square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" class file"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        sizetype = type(size)
+        if sizetype is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        sqreArea = self._Square__size ** 2
+        return sqreArea
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        sizetype = type(value)
+        if sizetype is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    def my_print(self):
+        leg = self._Square__size
+        if leg == 0:
+            print("")
+        else:
+            for i in range(leg):
+                for t in range(leg):
+                    print("#", end="")
+                print()
