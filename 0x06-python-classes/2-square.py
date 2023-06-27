d52995bf787bad62e7facf54db82a014e8ad23e2
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 01:33:52 +0200

    2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class file"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        sizetype = type(size)
+        if sizetype is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
