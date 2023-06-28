facc99f4f072fbf5d4f668c2c9fe438931aba692
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 04:02:57 +0200

    102-square

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" class file"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        sizetype = type(size)
+        if sizetype is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        sqreArea = self._Square__size ** 2
+        return sqreArea
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        sizetype = type(value)
+        if sizetype is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    def __ne__(self, other):
+        ope = self.area() != other.area()
+        return ope
+
+    def __eq__(self, other):
+        ope = self.area() == other.area()
+        return ope
+
+    def __le__(self, other):
+        ope = self.area() <= other.area()
+        return ope
+
+    def __lt__(self, other):
+        ope = self.area() < other.area()
+        return ope
+
+    def __ge__(self, other):
+        ope = self.area() >= other.area()
+        return ope
+
+    def __gt__(self, other):
+        ope = self.area() > other.area()
+        return ope
+    
