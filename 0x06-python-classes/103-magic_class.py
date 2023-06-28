b5611d0341bab47be9fe3344f0b11835ecc85f02
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 04:20:17 +0200

    103-magic_calass set radius to 0

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,6 +11,7 @@ class MagicClass:
         if radType is not int and radType is not float:
             raise TypeError("radius must be an number")
         self.__rad = rad
+        return
 
     def area(self):
         areaR = pi * (self.__rad ** 2)
