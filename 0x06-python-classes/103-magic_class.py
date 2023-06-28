5b06f0680b414c47e216ca416ca889db5da17791
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 04:09:19 +0200

    103-magic_calass __init__

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,8 +5,11 @@ import math
 
 class MagicClass:
     """magic class"""
-    def __init__(self):
-        pass
+    def __init__(self, rad=0):
+        radType = type(rad)
+        if radType is not int:
+            raise TypeError("radius must be an number")
+        self._rad = rad
 
     def area(self):
         pass
