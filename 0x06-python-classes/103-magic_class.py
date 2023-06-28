262d215bfea27e115d617767edb1c3fabd53fa4f
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 04:18:36 +0200

    103-magic_calass set radius to 0

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ class MagicClass:
     """magic class"""
     def __init__(self, rad=0):
         radType = type(rad)
-        self.__rad = 0
+        self._rad = 0
         if radType is not int and radType is not float:
             raise TypeError("radius must be an number")
         self._rad = rad
