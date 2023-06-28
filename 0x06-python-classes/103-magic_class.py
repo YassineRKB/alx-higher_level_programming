6fe7385ff6f0c060af888d6280e1dc265b53d276
Yassine Rakibi <yassine@arcraven.com>
2023-06-28 04:07:02 +0200

    103-magic_calass class structure

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" class file for MagicClass"""
+import math
+
+
+class MagicClass:
+    """magic class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+    def circumference(self):
+        pass
