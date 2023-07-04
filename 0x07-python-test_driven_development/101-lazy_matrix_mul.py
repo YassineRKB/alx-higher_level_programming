72b7ba2f87fbbcae9e2861d946969d83fcfe370c
Yassine Rakibi <yassine@arcraven.com>
2023-07-05 01:43:26 +0200

    holy 101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import numpy
+def lazy_matrix_mul(m_a, m_b):
+    res = numpy.matmul(m_a, m_b)
+    return res
---
+++ 0x07-python-test_driven_development/192-python.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+/**
+ * print_python_string - func to print a python string
+ * @p: string
+ * Return: VOID
+*/
+void print_python_string(PyObject *p)
+{
+		long int length = 0;
+
+		fflush();
+		printf("[.] string object info\n");
+		if (!PyUnicode_CheckExact(p))
+		{
+			printf("  [ERROR] Invalid String Object\n");
+			return;
+		}
+		length = PyUnicode_GET_LENGTH(p);
+		if (PyUnicode_IS_ASCII(p))
+			printf("  type: compact ascii\n");
+		else
+			printf("  type: compact unicode object\n");
+		printf("  length: %ld\n", length);
+		printf("  value: %S\n", PyUnicode_AsWideCharString(p, &length));
+}
