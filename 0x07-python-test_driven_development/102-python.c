25f3d342811a5be5b6b7cbabbacb053b2181280f
Yassine Rakibi <yassine@arcraven.com>
2023-07-05 18:07:54 +0200

    102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -8,18 +8,18 @@ void print_python_string(PyObject *p)
 {
 	long int length = 0;
 
-	fflush();
+	fflush(stdout);
 	printf("[.] string object info\n");
 	if (!PyUnicode_CheckExact(p))
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
-	length = PyUnicode_GET_LENGTH(p);
 	if (PyUnicode_IS_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
+	length = PyUnicode_GET_LENGTH(p);
 	printf("  length: %ld\n", length);
 	printf("  value: %S\n", PyUnicode_AsWideCharString(p, NULL));
 }
