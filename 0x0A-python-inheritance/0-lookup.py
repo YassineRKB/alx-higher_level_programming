c1161beb4b1f363092200b9abfcde28b4bb8d8ee
Yassine Rakibi <yassine@arcraven.com>
2023-07-11 01:19:16 +0200

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" funct to return list of atributes and methods"""
+""" module for func to return list of atributes and methods"""
 
 
 def lookup(obj):
+    """func to return list"""
     return dir(obj)
