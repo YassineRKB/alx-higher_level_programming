ee4afcf404031a31f5c6bd8685a99e04a85dc0e4
Yassine Rakibi <yassine@arcraven.com>
2023-07-11 01:25:07 +0200

    1-my_list.py module

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for calss Mylist"""
+
+
+class MyList(list):
+    """Class Mylist that iherits from list"""
+    def print_sorted(self):
+        """func to print sorted list"""
+        res = sorted(self)
+        print(res)
