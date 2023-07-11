d0e01c0018a6e9eaf7ef47503c24c971d9269425
Yassine Rakibi <yassine@arcraven.com>
2023-07-11 02:36:51 +0200

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module for calss BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty base class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
