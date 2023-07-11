b36104d6db506f96d10853b638d0f98ab0e7e069
Yassine Rakibi <yassine@arcraven.com>
2023-07-11 03:29:11 +0200

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module file for MyInt class"""
+
+
+class MyInt(int):
+    """MyInt rebel class"""
+    def __eq__(self, other):
+        return self.real != other
+    def __ne__(self, other):
+        return self.real == other
