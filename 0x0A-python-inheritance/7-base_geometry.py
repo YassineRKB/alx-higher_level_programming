052fe4ed8b15c55187cf18017d7484e0be07a023
Yassine Rakibi <yassine@arcraven.com>
2023-07-11 02:44:49 +0200

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,3 +6,11 @@ class BaseGeometry:
     """empty base class BaseGeometry"""
     def area(self):
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        msgType = "{} must be an integer".format(name)
+        msgValue = "{} must be greater than 0".format(name)
+        if type(value) is not int:
+            raise TypeError(msgType)
+        if value <= 0:
+            raise ValueError(msgValue)
