1d202a1b9a9fd1af5c5ec828299730049644fae3
Yassine Rakibi <yassine@arcraven.com>
2023-07-11 03:17:42 +0200

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module for calss BaseGeometry"""
+
+BG = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BG):
+    """empty base class BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        msg = "[{}] {} / {}".format(type(self).__name__, self.__width, self.__height)
+        return msg
+
+    def area(self):
+        res = self.__width * self.__height
+        return res
