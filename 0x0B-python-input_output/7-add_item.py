3e2f422c5ae979fdb14c9fa7f39d6a4383f65a03
Yassine Rakibi <yassine@arcraven.com>
2023-07-12 02:04:43 +0200

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,9 +7,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == "__main__":
-	try:
-		data = load_from_json_file("add_item.json")
-	except FileNotFoundError:
-		data = []
-	data.extend(sys.argv[1:])
-	save_to_json_file(data, "add_item.json")
+    try:
+        data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data = []
+    data.extend(sys.argv[1:])
+    save_to_json_file(data, "add_item.json")
