c521d9864638aa8af396280bcd72c2e42383311d
Yassine Rakibi <yassine@arcraven.com>
2023-07-12 02:13:13 +0200

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module student"""
+
+
+class Student:
+    """student class"""
+    
+    def __init__(self, firstname, lastname, age):
+        self.first_name = firstname
+        self.last_name = lastname
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
