d1aedee4f7c825a4d83860aacee4b7b36dae449c
Yassine Rakibi <yassine@arcraven.com>
2023-07-17 18:51:39 +0200

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,19 +8,23 @@ Rectangle = __import__("base").Rectangle
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
+        """constructor"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """getter: size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """setter: size"""
         super().valueChecker("width", value)
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """method: update"""
         lenarg = len(args)
         liargs = ["id", "size", "x", "y"]
         if args is not None and lenarg > 0:
