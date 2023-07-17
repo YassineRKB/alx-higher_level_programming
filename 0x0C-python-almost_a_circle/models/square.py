22569ecb3b1d1f163aa6f3df1706c155fc53e46f
Yassine Rakibi <yassine@arcraven.com>
2023-07-17 03:05:08 +0200

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,3 +10,22 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        super().valueChecker("width", value)
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        lenarg = len(args)
+        liargs = ["id", "size", "x", "y"]
+        if args is not None and lenarg > 0:
+            for i in range(lenarg):
+                setattr(self, liargs[i], args[i])
+        else:
+            for key in kwargs:
+                setattr(self, key, kwargs[key])
