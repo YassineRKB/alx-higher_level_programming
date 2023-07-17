397b273f71ba6b876d13f29aa3e538e75c43f49f
Yassine Rakibi <yassine@arcraven.com>
2023-07-17 18:42:16 +0200

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,48 +14,57 @@ class Rectangle(Base):
         self.valueChecker("x", x)
         self.valueChecker("y", y)
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
+        """getter: width"""
         return self.__width
 
     @property
     def height(self):
+        """getter: height"""
         return self.__height
 
     @property
     def x(self):
+        """getter: x"""
         return self.__x
 
     @property
     def y(self):
+        """getter: y"""
         return self.__y
 
     @width.setter
     def width(self, value):
+        """setter: width"""
         self.valueChecker("width", value)
         self.__width = value
 
     @height.setter
     def height(self, value):
+        """setter: height"""
         self.valueChecker("height", value)
         self.__height = value
 
     @x.setter
     def x(self, value):
+        """setter: x"""
         self.valueChecker("x", value)
         self.__x = value
 
     @y.setter
     def y(self, value):
+        """setter: y"""
         self.valueChecker("y", value)
         self.__y = value
 
     def valueChecker(self, var, val):
+        """method: value checker"""
         typeInt = isinstance(val, int)
         MsgMustBeInt = " must be integer"
         MsgMustBePos = " must be > 0"
@@ -68,10 +77,12 @@ class Rectangle(Base):
             raise ValueError(var + MsgMustbePosOrZero)
 
     def area(self):
+        """method: area"""
         resarea = self.width * self.height
         return resarea
 
     def display(self):
+        """method: display"""
         resoutput = "\n" * self.y
         for i in range(self.height):
             resoutput += " " * self.x
@@ -81,11 +92,13 @@ class Rectangle(Base):
         print(resoutput, end="")
 
     def __str__(self):
+        """method: str"""
         resstr = f"[{type(self).__name__}] ({self.id}) {self.x}/{self.y} \
             - {self.width}/{self.height}"
         return resstr
 
     def update(self, *args, **kwargs):
+        """method: update"""
         lenarg = len(args)
         liargs = ["id", "width", "height", "x", "y"]
         if args is not None and lenarg > 0:
