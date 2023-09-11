1ff5f4739e88edbe7e4eaf486f21ab59b230171a
Yassine Rakibi <yassine@arcraven.com>
2023-09-12 01:34:14 +0200

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = import('process');
+if (argv.lenght === 2) {
+  console.log('No arguments');
+} else if (argv.lenght === 3) {
+  console.log('Argument found');
+} else {
+  console.log('arguments found');
+}
