6000ff03147a0464c0501d4ff786890f03d5ee72
Yassine Rakibi <yassine@arcraven.com>
2023-09-12 01:59:51 +0200

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (!isNaN) {
+  console.log('Not a number');
+} else {
+  const line = 'My number: ' + parseInt(argv[2]);
+  console.log(line);
+}
