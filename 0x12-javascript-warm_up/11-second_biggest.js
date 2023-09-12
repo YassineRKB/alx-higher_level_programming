17647fcbafccb77f70056eb1e231bfc8ce505ee3
Yassine Rakibi <yassine@arcraven.com>
2023-09-12 02:44:04 +0200

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (!argv.length <= 3) {
+if (argv.length <= 3) {
+  console.log(0);
+} else {
   const numbers = argv.slice(2);
   numbers.sort(
     (a, b) => b - a
   );
   console.log(numbers[1]);
-} else {
-  console.log(0);
 }
