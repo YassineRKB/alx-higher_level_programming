d3ae95784d4e88e390d22f90cd458e3ecafcb0f5
Yassine Rakibi <yassine@arcraven.com>
2023-09-13 02:22:53 +0200

    task: 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    for (; i < this.height;) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+}
+module.exports = Rectangle;
