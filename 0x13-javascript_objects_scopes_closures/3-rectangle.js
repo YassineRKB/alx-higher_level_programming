a851b4759c5167ca656e07b4b7a423fead5b1228
Yassine Rakibi <yassine@arcraven.com>
2023-09-13 02:19:47 +0200

    task: 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
   print () {
     let column = 0;
     for (; col < this.height;) {
       console.log('X'.repeat(this.width));
       column += 1;
-    }
-  }
-  constructor (w, h) {
-    if (h > 0 && w > 0) {
-      this.width = w;
-      this.height = h;
     }
   }
 }
