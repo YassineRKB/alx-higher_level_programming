0a777794a3cef6ada667742a9797c8aa95c6231b
Yassine Rakibi <yassine@arcraven.com>
2023-09-13 02:50:28 +0200

    task: 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const list = require('100-data').list;
+const nList = list.map((x, index) => x * index);
 console.log(list);
-console.log(list.map((x, index) => x * index));
+console.log(nList);
