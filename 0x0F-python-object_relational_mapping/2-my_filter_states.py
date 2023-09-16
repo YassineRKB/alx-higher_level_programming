1891d750b72fea504dc1b42796cf5f61c8f538f6
Yassine Rakibi <yassine@arcraven.com>
2023-09-16 17:59:22 +0200

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script to list all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+
+    cursor = db.cursor()
+    querry = "SELECT * FROM states ORDER BY id WHERE name LIKE \
+        '{}' ORDER BY id ASC".format(argv[4])
+    cursor.execute(querry)
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
