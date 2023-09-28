eb74777316a3d4cb2f38911f4dd256595aaefd33
YassineRKB <yassine@arcraven.com>
2023-09-28 17:17:35 +0200

    task 0+1 after debugging

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script
+#Bash script to display the size of the body of the response after request to url
 curl -s $1 | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# bash script: send GET request to URL, then display response.body
+curl -sL "$1"
