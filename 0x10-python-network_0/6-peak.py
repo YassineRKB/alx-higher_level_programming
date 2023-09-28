c33c31081c443d464c6b52e88999dd4aee3f83a9
YassineRKB <yassine@arcraven.com>
2023-09-28 18:25:07 +0200

    task 6+ empty list causes error fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,21 +4,24 @@ Module to find a peak in a list of unsorted ints
 """
 
 
-def find_peak(list_of_integers, left=None, right=None):
+def find_peak(list_of_integers):
     """
     Find a peak in a list of unsorted integers using binary search.
     """
-    if not list_of_integers:
+    length = len(list_of_integers)
+    if length == 0:
         return None
-    if left is None:
-        left = 0
-    if right is None:
-        right = len(list_of_integers) - 1
-    mid = (left + right) // 2
-    if list_of_integers[mid] < list_of_integers[mid + 1]:
-        left = mid + 1
-    else:
-        right = mid
-    if left == right:
-        return list_of_integers[left]
-    return find_peak(list_of_integers, left, right)
+    elif length == 1 or list_of_integers[0] >= list_of_integers[1]:
+        return list_of_integers[0]
+    elif list_of_integers[length - 1] >= list_of_integers[length - 2]:
+        return list_of_integers[length - 1]
+    left, right = 1, length - 2
+    while left <= right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] >= list_of_integers[mid - 1] and \
+           list_of_integers[mid] >= list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        elif list_of_integers[mid - 1] > list_of_integers[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
