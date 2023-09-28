b0aa511fdfc3ed49248e8584e332bcf37db2ccb2
YassineRKB <yassine@arcraven.com>
2023-09-28 18:16:51 +0200

    task 6+ empty list causes error fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,21 +4,21 @@ Module to find a peak in a list of unsorted ints
 """
 
 
-def find_peak(list_of_integers):
+def find_peak(list_of_integers, left=None, right=None):
     """
     Find a peak in a list of unsorted integers using binary search.
     """
-    if list_of_integers == []:
+    if not list_of_integers:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    if len(list_of_integers) == 2:
-        return max(list_of_integers)
-    left, right = 0, len(list_of_integers) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-    return list_of_integers[left]
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(list_of_integers) - 1
+    mid = (left + right) // 2
+    if list_of_integers[mid] < list_of_integers[mid + 1]:
+        left = mid + 1
+    else:
+        right = mid
+    if left == right:
+        return list_of_integers[left]
+    return find_peak(list_of_integers, left, right)
