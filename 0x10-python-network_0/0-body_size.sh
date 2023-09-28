35e89e8d96cd17f7de9b4c440080a8ee4d62d1f9
YassineRKB <yassine@arcraven.com>
2023-09-28 17:09:31 +0200

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Bash script that takes in a URL, displays the size of the body of the response after request
 curl -Is "$1" | grep "Content-Length" | cut -d' ' -f2
