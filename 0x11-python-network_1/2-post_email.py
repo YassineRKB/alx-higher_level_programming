e79253474b2d55eefeffadca40b30a228637211d
YassineRKB <yassine@arcraven.com>
2023-09-30 00:39:29 +0200

    debugging checker

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,11 +5,11 @@ with the email as a parameter, and displays the
 body of the response (decoded in utf-8)"""
 import urllib.request
 import urllib.parse
-from sys import argv
-
+import sys
 if __name__ == "__main__":
-    mail = {"email": argv[2]}
+    mail = {"email": sys.argv[2]}
     encoding = urllib.parse.urlencode(mail).encode("ascii")
-    req = urllib.request.Request(argv[1], encoding)
+    req = urllib.request.Request(sys.argv[1], encoding)
     with urllib.request.urlopen(req) as res:
-        print(res.read().decode('utf8'))
+        data = res.read().decode('utf8')
+        print(data)
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,11 @@
 """Python script that takes in a URL, sends a request to the URL"""
 import urllib.request
 import urllib.error
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(argv[1]) as res:
+        with urllib.request.urlopen(sys.argv[1]) as res:
             data = res.read().decode("utf-8")
             print(data)
     except urllib.error.HTTPError as error:
