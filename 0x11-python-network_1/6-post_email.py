7743b931370a0c9dfef955f9ae3216582a2c30f0
YassineRKB <yassine@arcraven.com>
2023-09-30 01:43:48 +0200

    debugging checker

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,7 +2,7 @@
 """Python script that takes in a URL, sends a request to the
 URL and displays the value of the variable X-Request-Id in
 the response header"""
-from sys
+import sys
 import requests
 
 if __name__ == "__main__":
