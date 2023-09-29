af6301313d7f6b29bfa6a7063184c69f8693e556
YassineRKB <yassine@arcraven.com>
2023-09-30 00:33:51 +0200

    debugging checker

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,12 +3,13 @@
 sends a POST request to the passed URL
 with the email as a parameter, and displays the
 body of the response (decoded in utf-8)"""
-import urllib.request as quest
+import urllib.request
+import urllib.parse
 from sys import argv
 
 if __name__ == "__main__":
     mail = {"email": argv[2]}
-    encoding = quest.parse.urlencode(mail).encode("ascii")
-    req = quest.Request(argv[1], encoding)
-    with quest.urlopen(req) as res:
+    encoding = urllib.parse.urlencode(mail).encode("ascii")
+    req = urllib.request.Request(argv[1], encoding)
+    with urllib.request.urlopen(req) as res:
         print(res.read().decode('utf8'))
