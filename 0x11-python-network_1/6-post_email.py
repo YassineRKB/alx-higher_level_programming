76d0daf014a88ce1733c5434012ad27ee3e45b68
YassineRKB <yassine@arcraven.com>
2023-09-30 01:43:14 +0200

    debugging checker

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the
+URL and displays the value of the variable X-Request-Id in
+the response header"""
+from sys
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    json = {'email': sys.argv[2]}
+    data = requests.post(url, data=json).text
+    print(data)
