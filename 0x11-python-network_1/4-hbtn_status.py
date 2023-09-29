734c38dd8ab46554a3c89c9e83c744b4482a88b1
YassineRKB <yassine@arcraven.com>
2023-09-30 01:38:21 +0200

    debugging checker

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
-
-
 import requests
-
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status").text
