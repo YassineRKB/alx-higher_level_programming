f6c88ef598e976165e118da54fb66c0ac1319dbd
YassineRKB <yassine@arcraven.com>
2023-09-30 01:53:27 +0200

    debugging checker

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     data = requests.get("https://intranet.hbtn.io/status").text
     print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
