35ad0eb3855e6053267982de4bf7933b70e8d65d
YassineRKB <yassine@arcraven.com>
2023-09-29 23:29:57 +0200

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as quest
+
+if __name__ == "__main__":
+    with quest.urlopen('https://alx-intranet.hbtn.io/status') as req:
+        data = req.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
