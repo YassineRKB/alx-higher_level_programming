ae68f284131553240a8ca201d548c15573df4f6e
YassineRKB <yassine@arcraven.com>
2023-09-30 02:14:53 +0200

    task 9

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    data = requests.get("https://intranet.hbtn.io/status").text
+    data = requests.get('https://intranet.hbtn.io/status').text
     print("Body response:")
     print(f"\t- type: {type(data)}")
     print(f"\t- content: {data}")
