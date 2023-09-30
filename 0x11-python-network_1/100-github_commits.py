81c6f63db469f236f052ca1d74a7269f51cc84bf
YassineRKB <yassine@arcraven.com>
2023-09-30 02:37:38 +0200

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Python script that takes 2 arguments
 in order to solve this challenge."""
-import requests
 from sys import argv
+import requests
 
 if __name__ == "__main__":
-    repo = sys.argv[1]
-    owner = sys.argv[2]
+    repo = argv[1]
+    owner = argv[2]
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
     commits = requests.get(url)
     for commit in commits.json()[:10]:
