ace950f8ce3c28c6b088a48f0873863ca884c400
YassineRKB <yassine@arcraven.com>
2023-09-30 02:33:38 +0200

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     URL = f"https://api.github.com/repos/{argv[2]}/{argv[2]}/commits"
     req = requests.get(URL)
     totalcommits = req.json()
-    for commit in totalcommits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+    try:
+        for commit in totalcommits[:10]:
+            print(commit.get('sha'), end=': ')
+            print(commit.get('commit').get('author').get('name'))
+    except IndexError:
+        pass
