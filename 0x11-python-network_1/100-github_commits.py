1c9e011e8872656d4e3ac0ff2b011fb5bcfe892d
YassineRKB <yassine@arcraven.com>
2023-09-30 02:25:17 +0200

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments
+in order to solve this challenge."""
+import sys
+import requests
+
+if __name__ == "__main__":
+    owner = sys.argv[1]
+    repo = sys.argv[2]
+    URL = f"https://api.github.com/repos/{Owner}/{repo}/commits"
+    req = requests.get(URL)
+    total_commits = req.json()
+    for commit in total_commits[0:10]:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author}")
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,6 +2,7 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
+
 if __name__ == "__main__":
     data = requests.get('https://intranet.hbtn.io/status').text
     print("Body response:")
