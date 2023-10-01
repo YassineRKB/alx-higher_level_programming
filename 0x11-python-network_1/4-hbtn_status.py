0a28ebf9575f3fb956030c75934cb91aaa6d60a4
Yassine Rakibi <yassine@arcraven.com>
2023-10-01 16:51:29 +0200

    end of project

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    data = requests.get('https://intranet.hbtn.io/status').text
+    data = requests.get('https://alx-intranet.hbtn.io/status').text
     print("Body response:")
     print(f"\t- type: {type(data)}")
     print(f"\t- content: {data}")
