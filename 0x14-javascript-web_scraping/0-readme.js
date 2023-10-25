ed8a4be1ef13fad921b03290347660a2bdbdf0b2
YassineRKB <yassine@arcraven.com>
2023-10-25 03:12:15 +0200

    task 0: fix perm

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,10 @@
 const stream = require('fs');
 
 const file = process.argv[2];
-stream.readFile(file, 'utf8', (err, data) => {
+stream.readFile(
+  file,
+  'utf8',
+  (err, data) => {
   if (err) {
     console.log(err);
   } else {
