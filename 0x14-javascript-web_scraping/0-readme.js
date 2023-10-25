469e031cc8533c8065a04abf4025918f3b6c3adf
YassineRKB <yassine@arcraven.com>
2023-10-25 03:06:15 +0200

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,7 @@
 const stream = require('fs');
 
 const file = process.argv[2];
-stream.readFile(
-  file,
-  'utf8',
-  (err, content) => {
+stream.readFile(file, 'utf8', (err, content) => {
   if (err) {
     console.log(err);
   } else {
