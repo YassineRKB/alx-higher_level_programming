a73b1645ed4f3de5315293ebd8deb84405f24b23
YassineRKB <yassine@arcraven.com>
2023-10-25 03:42:59 +0200

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+request(url,
+  { method: 'GET' },
+  (err, { statusCode }) => {
+    if (err) return console.log(err);
+    console.log(`code: ${statusCode}`);
+  });
