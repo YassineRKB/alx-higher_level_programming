d8788d1718c7c2bf985a963050cc67911ce1a724
YassineRKB <yassine@arcraven.com>
2023-10-25 03:04:44 +0200

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,9 @@ stream.readFile(
   file,
   'utf8',
   (err, content) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(content);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });
