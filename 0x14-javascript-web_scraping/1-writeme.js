e923b6d277bf1272c1ab217d965611c8d6c8c183
YassineRKB <yassine@arcraven.com>
2023-10-25 03:26:58 +0200

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,6 @@ const stream = require('fs');
 
 const file = process.argv[1];
 const data = process.argv[2];
-stream.writeFile(
-  file,
-  data,
-  'utf-8',
-  (err) => {
-    if (err) console.log(err.message);
-  });
+stream.writeFile(file, data, 'utf-8', (err) => {
+  if (err) console.log(err.message);
+});
